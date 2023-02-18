7694b5ed96cddfd192b280d8b10e5fa7d502c00f
Oluwatoyosi <oladipooluwatoyosi@yahoo.com>
2023-02-18 21:00:04 +0100

    initial commit

---
+++ component/Icon.js
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+function IconChevronDoubleRight(props) {
+  return (
+    <svg
+      fill="currentColor"
+      viewBox="0 0 16 16"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        d="M3.646 1.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L9.293 8 3.646 2.354a.5.5 0 010-.708z"
+      />
+      <path
+        fillRule="evenodd"
+        d="M7.646 1.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L13.293 8 7.646 2.354a.5.5 0 010-.708z"
+      />
+    </svg>
+  );
+}
+
+export default IconChevronDoubleRight;
